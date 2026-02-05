b44fd0319da204c65c98e92a8c6f491412c19473
Loïc Bazonnais <loic.bazonnais@gmail.com>
2026-02-05 15:29:49 +0100

    add message init done

--- Projects/NUCLEO-L073RZ/Applications/LoRaWAN/LoRaWAN_AT_Master/Core/Src/main.c
+++ Projects/NUCLEO-L073RZ/Applications/LoRaWAN/LoRaWAN_AT_Master/Core/Src/main.c
@@ -30,6 +30,7 @@
 #include "app_master.h"
 #include <stdarg.h>
 #include "rtc_if.h"
+#include "lrwan_ns1_printf.h"
 
 #include ATCMD_MODEM        /* preprocessing definition in sys_conf.h*/
 
@@ -81,7 +82,8 @@ int main(void)
 
   // 0. TODO LORA USE_LRWAN_NS1: Print "init done" on console
   // 0. TODO LORA USE_LRWAN_NS1: hint: use dbg_printf_send() (where is it? how does it work?)
- 
+  dbg_printf_send("[INIT DONE]\n");
+
   /* Infinite loop */
   while (1)
   {
